56b5ea929adfa3cb8bca89c28eb57415f9ee22fd
ne18070 <ne18070@azec.ac.jp>
2019-11-14 10:33:29 +0900

    Change the HTML for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,7 +114,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
